37516faa5114798f84890e33f2907c6b2e4fb078
dianamemmedova <dianamemmedova653@gmail.com>
2026-07-20 12:05:42 +0400

    create services part

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+     {path:'' ,  pathMatch: 'full', redirectTo: 'home' },
     {path:'home' , component: HomeComponent }
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule , MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
